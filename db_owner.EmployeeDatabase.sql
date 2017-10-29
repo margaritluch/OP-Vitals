a0da4e12ecd9d942942a0db736a3c9d8e90ac858
Maiken Guldberg <maiken.g@fibermail.dk>
2017-10-29 22:58:34 +0100

    Databaserne er sat op

---
+++ db_owner.EmployeeDatabase.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [db_owner].[EmployeeDatabase] (
+    [EmployeeID]        NCHAR (6)     NOT NULL,
+    [PassWord]          NVARCHAR (10) NOT NULL,
+    [EmployeeFirstName] NVARCHAR (10) NOT NULL,
+    [EmployeeLastName]  NVARCHAR (10) NOT NULL,
+    [Profession]        NVARCHAR (10) NOT NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeID] ASC)
+);
+
