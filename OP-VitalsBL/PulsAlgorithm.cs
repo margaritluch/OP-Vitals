a4f2686cb9cff70e725f90ad3cfd89be1eb0d9bc
Margarit Ghubatyan <au534498@uni.au.dk>
2017-11-12 22:43:02 +0100

    Dias algorithm rettet +puls algorithm +digital filter klasse

--- OP-VitalsBL/PulsAlgorithm.cs
+++ OP-VitalsBL/PulsAlgorithm.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTO;
 
 namespace OP_VitalsBL
 {
     public class PulsAlgorithm
     {
+        private SysAlgorithm syses;
 
+        public int pulseCalculation(DAQSettingsDTO daqdto)
+        {
+            syses = new SysAlgorithm();
+
+            int sampleInSeconds = daqdto.Samples / daqdto.SampleRate;
+            int calcFactor = 60 / sampleInSeconds;
+
+            int pulse = syses.CalcSystolic(syses.Findmaxbaseline(daqdto)).Count * calcFactor;
+
+            return pulse;
+        }
     }
 }
